361b8a577b930c116774b68d1dfe75cf70e5420d
mrPichon <warcraftisra4@gmail.com>
2023-02-06 23:36:18 -0800

    module 3 good with help :)

--- alog.go
+++ alog.go
@@ -29,20 +29,30 @@ func New(w io.Writer) *Alog {
 		w = os.Stdout
 	}
 	return &Alog{
-		dest:    w,
-		msgCh:   make(chan string),
-		errorCh: make(chan error),
-		m:       new(sync.Mutex),
+		dest:               w,
+		msgCh:              make(chan string),
+		errorCh:            make(chan error),
+		m:                  &sync.Mutex{}, //new(sync.Mutex),
+		shutdownCh:         make(chan struct{}),
+		shutdownCompleteCh: make(chan struct{}),
 	}
 }
 
 // Start begins the message loop for the asynchronous logger. It should be initiated as a goroutine to prevent
 // the caller from being blocked.
 func (al Alog) Start() {
-	for msg := range al.msgCh {
-		go func(m string) {
-			al.write(m, nil)
-		}(msg)
+	wg := &sync.WaitGroup{}
+loop:
+	for {
+		select {
+		case msg := <-al.msgCh:
+			wg.Add(1)
+			al.write(msg, wg)
+		case <-al.shutdownCh:
+			wg.Wait()
+			al.shutdown()
+			break loop
+		}
 	}
 }
 
@@ -54,19 +64,20 @@ func (al Alog) formatMessage(msg string) string {
 }
 
 func (al Alog) write(msg string, wg *sync.WaitGroup) {
-	fmsg := al.formatMessage(msg)
+	defer wg.Done()
 	al.m.Lock()
 	defer al.m.Unlock()
-	_, err := al.dest.Write([]byte(fmsg))
+	_, err := al.dest.Write([]byte(al.formatMessage(msg)))
 	if err != nil {
-		go func(e error) {
-			al.errorCh <- e
+		go func(err error) {
+			al.errorCh <- err
 		}(err)
 	}
-
 }
 
 func (al Alog) shutdown() {
+	close(al.msgCh)
+	al.shutdownCompleteCh <- struct{}{}
 }
 
 // MessageChannel returns a channel that accepts messages that should be written to the log.
@@ -84,6 +95,8 @@ func (al Alog) ErrorChannel() <-chan error {
 // Stop shuts down the logger. It will wait for all pending messages to be written and then return.
 // The logger will no longer function after this method has been called.
 func (al Alog) Stop() {
+	al.shutdownCh <- struct{}{}
+	<-al.shutdownCompleteCh
 }
 
 // Write synchronously sends the message to the log output
